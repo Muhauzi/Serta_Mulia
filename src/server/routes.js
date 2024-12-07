672bc01c80fdd3c01158d8d7067fdffe955a5f96
Sania <saniafitria12@gmail.com>
2024-12-07 12:28:45 +0700

    First Commit

---
+++ src/server/routes.js
@@ -0,0 +1,24 @@
+const postPredictHandler = require('../server/handler');
+     
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  },
+  {
+    path: '/',
+    method: 'GET',
+    handler: (request, h) => {
+        return 'Index Route';
+    }
+  }
+]
+ 
+module.exports = routes;
